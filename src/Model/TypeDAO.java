cf7c95fff9dc3dcfda7f7504f9418a6b9c71dd56
edu06 <edu06@EDUPC06.n00d01.kis.co.jp>
2020-06-02 14:11:38 +0900

    6.2

---
+++ src/Model/TypeDAO.java
@@ -0,0 +1,42 @@
+package Model;
+
+import java.util.ArrayList;
+
+	/**
+	 * 種類のDAO
+	 * @author edu06
+	 *
+	 */
+public class TypeDAO {
+
+	/**
+	 * 種類の全検索
+	 * @return ペット種類のリスト
+	 */
+	public ArrayList<Type> findAll() {
+
+	}
+
+	/**
+	 * tidによる種類の検索
+	 * @return 種類情報
+	 */
+
+	public ArrayList<Type> findByTid() {
+
+	}
+
+	/**
+	 * ペット種類の追加
+	 */
+	public void insert() {
+
+	}
+
+	/**
+	 * ペット種類の削除
+	 */
+	public void delete() {
+
+	}
+}
