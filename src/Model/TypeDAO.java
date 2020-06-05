4f181be943a12e8705d7f02176670479ffddf9b8
edu07 <edu07@EDUPC07.n00d01.kis.co.jp>
2020-06-05 11:53:44 +0900

    7.1

--- src/Model/TypeDAO.java
+++ src/Model/TypeDAO.java
@@ -1,5 +1,10 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 	/**
@@ -8,13 +13,40 @@ import java.util.ArrayList;
 	 *
 	 */
 public class TypeDAO {
+	static final String URL =  "jdbc:mysql://localhost/m_pet?useSSL=false";
+	static final String USER = "java";
+	static final String PASS = "pass";
 
 	/**
 	 * 種類の全検索
 	 * @return ペット種類のリスト
 	 */
 	public ArrayList<Type> findAll() {
+		ArrayList<Type> tlist = new ArrayList<Type>();
+		try (Connection con = DriverManager.getConnection (URL,USER,PASS);){
 
+
+			String sql = "SELECT * FROM m_pet;";
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			ResultSet rs = stmt.executeQuery();
+
+			while(rs.next()) {
+				int tid = rs.getInt("tid");
+				String tname = rs.getString("tname");
+
+
+				Type t = new Type(tid, tname);
+				tlist.add(t);
+			}
+
+			stmt.close();
+
+		}
+		catch(SQLException e) {
+			System.out.println("findAllエラー：" + e.getMessage());
+		}
+		return tlist;
 	}
 
 	/**
@@ -23,7 +55,29 @@ public class TypeDAO {
 	 */
 
 	public Type findByTid(int tid) {
+		Type t = null;
+		try (Connection con = DriverManager.getConnection (URL,USER,PASS);){
 
+
+			String sql = "SELECT * FROM m_type WHERE tid = ?;";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, tid);
+
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				String tname = rs.getString("tname");
+
+
+				t = new Type(tid,tname);
+			}
+
+			stmt.close();
+
+		}
+		catch(SQLException e) {
+			System.out.println("findBySid：" + e.getMessage());
+		}
+		return t;
 	}
 
 	/**
