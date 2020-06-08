e0acd98c1cdfe536071c1af5e6d72635e84d9839
edu11 <edu11@EDUPC11.n00d01.kis.co.jp>
2020-06-08 10:43:12 +0900

    UserDAOのクラス図修正

--- src/Model/UserDAO.java
+++ src/Model/UserDAO.java
@@ -1,18 +1,55 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * ユーザーのDAO
- * @author edu06
+ * @author edu11
  *
  */
 public class UserDAO {
 
+	static final String URL = "jdbc:mysql://localhost/club?useSSL=false";
+	static final String USER = "Java";
+	static final String PASS = "pass";
+
+
 	/**
-	 * unameによるユーザーの検索
+	 * ユーザー名とパスワードによるユーザーのログイン
 	 * @return ユーザー
 	 */
-	public Booking findByUname() {
+	public User login(String uname, String pass) {
+		User u = null;
+
+		try (Connection con = DriverManager.getConnection(URL, USER, PASS);) {
+
+			String sql = "SElECT * FROM user WHERE uname = ? and pass = ?";
+
+
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			stmt.setString(1, uname);
+			stmt.setString(2, pass);
+
+			ResultSet rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				int uid = rs.getInt("uid");
+				String mail = rs.getString("mail");
+
+				u = new User(uid, uname, pass, mail);
+
+			}
+			stmt.close();
+
+		} catch (SQLException e) {
+			System.out.println("loginエラー：" + e.getMessage());
+		}
+		return u;
 
 	}
 }
