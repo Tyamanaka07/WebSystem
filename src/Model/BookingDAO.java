30f59f2413ee62448ce267610b29a91836f496cc
edu07 <edu07@EDUPC07.n00d01.kis.co.jp>
2020-06-10 15:20:59 +0900

    SQL変更

--- src/Model/BookingDAO.java
+++ src/Model/BookingDAO.java
@@ -31,7 +31,7 @@ public class BookingDAO {
 
 		try (Connection con = DriverManager.getConnection(URL, USER, PASS);) {
 
-			String sql = "SELECT * FROM m_booking;";
+			String sql = "SELECT * FROM m_booking ORDER BY booking ASC;";
 			PreparedStatement stmt = con.prepareStatement(sql);
 
 			ResultSet rs = stmt.executeQuery();
